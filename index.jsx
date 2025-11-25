fcace4d64fa820048bb645840df02f4cb422cc3e
Chelvin16 <71332242+Chelvin16@users.noreply.github.com>
2025-11-25 21:04:25 +0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar6 } from "./components/Navbar6";
+import { Header26 } from "./components/Header26";
+import { Layout13 } from "./components/Layout13";
+import { Layout394 } from "./components/Layout394";
+import { Layout16 } from "./components/Layout16";
+import { Gallery10 } from "./components/Gallery10";
+import { Contact13 } from "./components/Contact13";
+import { Testimonial17 } from "./components/Testimonial17";
+import { Footer1 } from "./components/Footer1";
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar6 />
+      <Header26 />
+      <Layout13 />
+      <Layout394 />
+      <Layout16 />
+      <Gallery10 />
+      <Contact13 />
+      <Testimonial17 />
+      <Footer1 />
+    </div>
+  );
+}
